9bc415ba86d064584623d3e9ff552dd3cf516204
Thomas Imensek <thomas.imensek@bergfex.at>
2021-08-11 19:33:33 +0200

    init middleware;

--- app/Http/Middleware/HandleInertiaRequests.php
+++ app/Http/Middleware/HandleInertiaRequests.php
@@ -37,7 +37,11 @@ class HandleInertiaRequests extends Middleware
     public function share(Request $request)
     {
         return array_merge(parent::share($request), [
-            //
+            'csrf_token' => csrf_token(),
+            'flash' => [
+                'success' => fn () => $request->session()->get('success')
+            ],
+            'auth.user' => fn () => $request->user() ? $request->user()->only('id', 'name', 'email') : null
         ]);
     }
 }
