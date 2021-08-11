564370623b1c7e4177f49f48833cabb112b51644
Thomas Imensek <thomas.imensek@bergfex.at>
2021-08-11 19:36:23 +0200

    adding Login + Partial-Components;

--- app/Http/Controllers/Auth/LoginController.php
+++ app/Http/Controllers/Auth/LoginController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers\Auth;
 
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
 use Inertia\Inertia;
 
 class LoginController extends Controller
@@ -15,11 +16,24 @@ class LoginController extends Controller
 
     public function authenticate(Request $request)
     {
-
+        $credentials = $this->validate($request, [
+            'email' => ['required', 'email'],
+            'password' => ['required']
+        ]);
+        if(Auth::attempt($credentials)) {
+            $request->session()->regenerate();
+            return redirect()->intended('/');
+        }
+        return back()->withErrors([
+            'email' => 'The provided credentials do not match our records.',
+        ]);
     }
 
     public function logout(Request $request)
     {
-
+        Auth::logout();
+        $request->session()->invalidate();
+        $request->session()->regenerateToken();
+        return redirect('/');
     }
 }
